c8717e9f88d5a2c47cdfdc13a20e8f0877e86f15
l3ch3 <109975455+l3ch3@users.noreply.github.com>
2023-02-09 14:29:52 +0000

    Fixed state update issue
    error caused by map running when there was no initial state to map

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,18 +3,30 @@ import recipes from "../assets/recipes";
 import { useState } from "react";
 
 export default function Main() {
-  const [recipe, setRecipe] = useState();
+  function getRandomRecipe() {
+    const randomNumber = Math.floor(Math.random() * recipes.length);
+    const randomRecipe = recipes[randomNumber];
+    return randomRecipe;
+    //remember to return
+  }
+  const [recipe, setRecipe] = useState(getRandomRecipe());
+
+  const recipeMethod = () =>
+    recipe.Method.map((step) => (
+      <p key={step}>
+        {recipe.Method.indexOf(step) + 1}. {step}
+      </p>
+    ));
+  //need to be function else it will always run and will throw an error at the inital state when
+  // recipe = null
+
+  const recipeIngredients = () =>
+    recipe.Ingredients.map((Ingredient) => (
+      <p key={Ingredient}>{Ingredient}</p>
+    ));
 
   function getRecipe() {
-    let randomNumber = Math.floor(Math.random() * recipes.length);
-    let randomRecipe = recipes[randomNumber];
-    let recipeIngredient = recipe.Ingredients.map((Ingredient) => (
-      <p>{Ingredient}</p>
-    ));
-    //why getting an error
-
-    setRecipe(randomRecipe);
-    console.log(recipe.Ingredients);
+    setRecipe(getRandomRecipe());
   }
 
   return (
@@ -23,21 +35,25 @@ export default function Main() {
         Random Recipe
       </button>
 
-      <div className="recipe">
-        <h2 className="recipe-title">{recipe.Name} </h2>
-        {/* <h2 className="recipe-title">{randomRecipe} </h2> 
-        error - undifiened */}
-        <hr></hr>
-        <p className="recipe-description">{recipe.Description} </p>
-        <div>
-          <h4>Ingredients</h4>
-          <p>{recipe.Ingredients}</p>
+      {recipe && ( // if recipe is not Null - run this statement
+        <div className="recipe">
+          <a href={recipe.url} target="_blank">
+            <h2 className="recipe-title">{recipe.Name}</h2>
+          </a>
+          <hr></hr>
+          <p className="recipe-description">{recipe.Description} </p>
+          <div>
+            <h4>{`(${recipe.Ingredients.length}) Ingredients`}</h4>
+            {/* <p>{recipeIngredients()}</p> */}
+            <p>{recipeIngredients()}</p>
+          </div>
+          <div className="recipe-method">
+            <h4>Method</h4>
+            <p>{recipeMethod()}</p>
+            {/* dont forget to call the function  */}
+          </div>
         </div>
-        <div className="recipe-method">
-          <h4>Method</h4>
-          <p>{recipe.Method}</p>
-        </div>
-      </div>
+      )}
     </main>
   );
 }
