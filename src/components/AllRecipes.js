f613a2a1917d7ed0ee88541230ca516f5c61a7d4
l3ch3 <109975455+l3ch3@users.noreply.github.com>
2023-02-21 12:28:41 +0000

    Added recipe count
    introduced to useEffect

--- src/components/AllRecipes.js
+++ src/components/AllRecipes.js
@@ -1,38 +1,36 @@
-import React from "react";
-import { useState } from "react";
+import React, { useDeferredValue } from "react";
+import { useState, useEffect } from "react";
 
 export default function ({ recipes, input }) {
-  const [recipeNo, setRecipeNo] = useState(recipes.length);
+  const [filteredRecipes, setFilteredRecipes] = useState(recipes);
 
-  console.log(recipes.arrayRef);
+  useEffect(() => {
+    const filtered = recipes.filter((recipe) => {
+      // receipe refers to each object in the array- look at filter parameters
+      if (input === "") return recipe;
+      else if (recipe.Name.toLowerCase().includes(input.toLowerCase())) {
+        return recipe;
+      }
+      return null; // requires null when both statements do not run
+    }); // new array is created
+    setFilteredRecipes(filtered);
+  }, [input, recipes]); //run useEffect when depenecies change
+  //dont update the depencies within the useEffect
 
   return (
     <div>
-      <p>{recipeNo} Recipes</p>
-      {recipes
-        .filter((recipe) => {
-          // receipe refers to each object in the array- look at filter parameters
-          if (input === "") return recipe;
-          else if (recipe.Name.toLowerCase().includes(input.toLowerCase())) {
-            return recipe;
-          }
-          return null; // requires null when both statements do not run
-        }) // new array is created
-        .map(
-          // maps the new array of the  filtered recipes
-          (
-            retrunedRecipe, //returnedRecipe is the name given to each object
-            index,
-            arrayRef // map (element, index, array) -> use index for the key
-          ) => {
-            return (
-              <p key={index}>
-                {retrunedRecipe.Name} {arrayRef.length}
-              </p>
-            );
-            //add bootstrap dropdown here
-          }
-        )}
+      <p>{filteredRecipes.length} Recipes</p>
+      {filteredRecipes.map(
+        // maps the new array of the  filtered recipes
+        (
+          retrunedRecipe, //returnedRecipe is the name given to each object
+          index
+          // map (element, index, array) -> use index for the key
+        ) => {
+          return <p key={index}>{retrunedRecipe.Name}</p>;
+          //add bootstrap dropdown here
+        }
+      )}
     </div>
   );
 }
